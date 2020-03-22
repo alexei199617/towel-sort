14321146d4913dbc5b93eefe701b9d5b6118e826
alexei199617 <alexei199318@mail.ru>
2020-03-22 23:35:14 +0300

    feat: add solution

---
+++ src/index.js
@@ -0,0 +1,17 @@
+module.exports = function towelSort(matrix) {
+    if (!matrix) return [];
+    let newArr = [];
+
+    for (let i = 0; i < matrix.length; i++) {
+        if (i > 0 && i % 2) {
+            for (let j = matrix[i].length - 1; j > -1; j--) {
+                newArr.push(matrix[i][j])
+            }
+        } else {
+            for (let j = 0; j < matrix[i].length; j++) {
+                newArr.push(matrix[i][j])
+            }
+        }
+    }
+    return newArr;
+}
